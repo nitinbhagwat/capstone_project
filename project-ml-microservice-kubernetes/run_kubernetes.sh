f8c72c7e5aae4d7417daf72e843ab2e09f011e4a
nitinbhagwat <n.bhagwat123@gmail.com>
2023-04-28 07:47:14 +0530

    Project 4 first commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=nitinbhagwat/udacity_devops:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+# Assuming the Kubernetes cluster is ready
+kubectl get nodes
+# Deploy an App from the Dockerhub to the Kubernetes Cluster
+kubectl create deploy udacity-devops --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+# See the status
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/udacity-devops-78585497b8-8qqf5 --address 0.0.0.0 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nitinbhagwat/udacity_devops
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=nitinbhagwat
+docker tag uda_devops $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
